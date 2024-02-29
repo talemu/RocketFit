2fd047116411162b8ff749b7986a083336f3fc90
Tabor Alemu <tabor.alemu@avanade.com>
2024-02-29 16:57:33 -0600

    cleaned up main useeffects, added valid auth id check to all pages

--- rocket-fit-front/client/src/pages/WorkoutPage.tsx
+++ rocket-fit-front/client/src/pages/WorkoutPage.tsx
@@ -1,23 +1,44 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 // import DayTable from "../components/DayTable";
 import styled from "styled-components";
 import DayTable from "../components/DayTable";
 import { WorkoutItem } from "../services/workoutExerciseService";
+import { useEffect } from "react";
 
-const WorkoutPage = () => {
+const BackToHomeButton = styled.button``;
+
+interface Props {
+  authId: number;
+}
+
+const WorkoutPage = ({ authId }: Props) => {
+  const Navigate = useNavigate();
   const location = useLocation();
   const item = location.state;
+  const validAuthIdShow = authId != -10;
 
-  const BackToHomeButton = styled.button``;
+  console.log(authId);
+
+  useEffect(() => {
+    if (authId == -10) {
+      Navigate("/main");
+    }
+  });
 
   return (
     <>
-      <Link to="/main" state={item[0]}>
-        <BackToHomeButton>Home</BackToHomeButton>
-      </Link>
-      {item[1].map((item2: WorkoutItem) => (
-        <DayTable item={item2} id={item[2]} workoutNum={item[3]} />
-      ))}
+      {validAuthIdShow ? (
+        <>
+          <Link to="/main" state={item[0]}>
+            <BackToHomeButton>Home</BackToHomeButton>
+          </Link>
+          {item[1].map((item2: WorkoutItem) => (
+            <DayTable item={item2} id={item[2]} workoutNum={item[3]} />
+          ))}
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 };
